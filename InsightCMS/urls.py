dfdd5e17fcdd364f3ae45144068df89f15e14806
tcq-pawas <pawas.singh@thecodiq.com>
2026-08-22 18:10:59 +0530

    i

--- InsightCMS/urls.py
+++ InsightCMS/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     path('', include(wagtail_urls)),  
 ]
-
+ 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
